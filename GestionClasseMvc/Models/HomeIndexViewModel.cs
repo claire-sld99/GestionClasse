efa980c034d5e180fac7b68a6c2d77b4b7061fa8
Claire SALAMAND <clairesalamand38@hotmail.fr>
2021-06-16 15:21:34 +0200

    page accueil

--- GestionClasseMvc/Models/HomeIndexViewModel.cs
+++ GestionClasseMvc/Models/HomeIndexViewModel.cs
@@ -1,1 +1,11 @@
 using System.Collections.Generic;
+using Ifinfo.Shared;
+
+namespace GestionClasseMvc.Models
+{
+    public class HomeIndexViewModel
+    {
+        public IList<Classe> Classes {get; set;}
+        public IList<Eleve> Eleves {get; set;}
+    }
+}
