e0de86237bfd91b1aafd3d81d10a0db0beffa7d7
Simon Gonzalez <gonzalezsimon@free.fr>
2021-06-17 10:24:57 +0200

    Changement elevedetail listedetail

---
+++ GestionClasseMvc/Models/EleveDetailViewModel.cs
@@ -0,0 +1,14 @@
+using Ifinfo.Shared;
+using System.Collections.Generic;
+
+namespace GestionClasseMvc.Models
+{
+    public class EleveDetailViewModel
+    {
+        public Eleve Eleve {get;set;}
+
+        public bool HasErrors {get;set;}
+
+        public IEnumerable<string> ValidationErrors {get;set;}
+    }
+}
