30efe2594daa2e5dfe6864474efdf2342c094322
Sben65 <sbenahben@gmail.com>
2021-06-18 10:48:23 +0200

    merge conflict

--- GestionClasseMvc/Controllers/HomeController.cs
+++ GestionClasseMvc/Controllers/HomeController.cs
@@ -105,5 +105,28 @@ namespace GestionClasseMvc.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult AjoutEvaluation(long id, Evaluation evaluation)
+        {
+            var rand = new Random();
+            if(ModelState.IsValid)
+            {
+                List<string> messages = new List<string>();
+                evaluation.ProfesseurID = rand.Next(1, db.Professeurs.Count()+1);
+                evaluation.EvaluationID = db.Evaluations.Count()+1;
+                evaluation.ClasseID = id;
+                db.Evaluations.Add(evaluation);
+                var affected = db.SaveChanges();
+                if(affected == 1){
+                    var model = new AjoutEvaluationViewModel();
+                    messages.Add("L'évaluation à bien été créé !");
+                    model.ValidationMessages = messages;
+                return View(model);
+                }
+
+            }
+            return View();
+        }
     }
 }
